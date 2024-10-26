a9fd63e1125480cf1171e47178525d95953b6f5e
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2024-10-26 20:53:27 -0300

    exibe mensagem de boas vindas + nome do usuário na landing page

--- aluguel-quadras-front/app/login/page.tsx
+++ aluguel-quadras-front/app/login/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { useAuth } from '../shared/auth/auth-context'
+import { AuthProvider, useAuth } from '../shared/auth/auth-context'
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
